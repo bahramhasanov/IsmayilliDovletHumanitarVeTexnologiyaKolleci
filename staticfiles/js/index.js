736730299f541f01dc458df5dc1a3cf582fe7bf0
vusallyv <vusal581176@gmail.com>
2021-11-30 20:59:53 +0400

    Slider 90%

--- staticfiles/js/index.js
+++ staticfiles/js/index.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+
+
     $('.owl-carousel').owlCarousel({
         mouseDrag: false,
         loop: true,
@@ -13,7 +15,7 @@ $(document).ready(function () {
                 items: 1
             },
             1000: {
-                items: 3
+                items: 5
             }
         }
     });
@@ -24,6 +26,7 @@ $(document).ready(function () {
         $('.owl-item .item').removeClass('next');
         $('.owl-item .item').removeClass('prev');
         $active.addClass('next');
+        $items = $('.owl-item .item');
         if ($active.is('.first')) {
             $('.owl-item .last').addClass('show');
             $('.first').addClass('next');
@@ -33,11 +36,22 @@ $(document).ready(function () {
             $active.parent().prev().children('.item').addClass('show');
             if ($active.parent().prev().children('.item').is('.first')) {
                 $('.owl-item .last').addClass('prev');
+                $firsts = $('.owl-item .item.first')
+                $firsts.removeClass('prev');
+                $firsts.addClass('show');
             }
             else {
                 $('.owl-item .show').parent().prev().children('.item').addClass('prev');
             }
         }
+        $items.each(function () {
+            if ($(this).is('.show')) {
+                $(this).removeClass('prev');
+            }
+            else {
+                $(this).addClass('prev');
+            }
+        });
     });
 
     $('.owl-next').click(function () {
@@ -45,6 +59,7 @@ $(document).ready(function () {
         $('.owl-item .item.show').removeClass('show');
         $('.owl-item .item').removeClass('next');
         $('.owl-item .item').removeClass('prev');
+        $('.owl-item .item').addClass('prev');
         $active.addClass('prev');
         if ($active.is('.last')) {
             $('.owl-item .first').addClass('show');
@@ -59,6 +74,7 @@ $(document).ready(function () {
                 $('.owl-item .show').parent().next().children('.item').addClass('next');
             }
         }
+        $('.owl-item .item.show').removeClass('prev');
     });
 
 });
