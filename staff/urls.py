d20480afbc30b7d7f74a6164be3c557c99137b6c
vusallyv <vusal581176@gmail.com>
2021-12-09 23:59:11 +0400

    PDF initial

--- staff/urls.py
+++ staff/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("director/", views.Director.as_view(), name="director"),
     path('teachers/', views.TeacherListView.as_view(), name='teachers'),
     # path('teachers/<int:pk>', views.TeacherListView.as_view(), name='teacher'),
+    path('library/', views.LibraryListView.as_view(), name='library'),
+    path('library/<int:pk>', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
 
--- staff/views.py
+++ staff/views.py
@@ -1,9 +1,10 @@
+from django.http import FileResponse, Http404
 from django.views.generic import ListView
 from django.shortcuts import render
 from django.views.generic import ListView, View
 
 
-from staff.models import Teacher
+from staff.models import PDF, Teacher
 
 
 class Deputies(View):
@@ -31,3 +32,26 @@ class TeacherListView(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Teachers'
         return context
+
+
+class LibraryListView(ListView):
+    model = PDF
+    template_name = 'library.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Onlayn Kitabxana'
+        context['pdfs'] = PDF.objects.all()
+        return context
+
+
+class LibraryDetailView(View):
+    def get(self, request, pk):
+        try:
+            pdf = PDF.objects.get(pk=pk)
+        except PDF.DoesNotExist:
+            raise Http404("File does not exist")
+        response = FileResponse(open(pdf.file.path, 'rb'))
+        response['Content-Disposition'] = 'inline; filename="{}"'.format(
+            pdf.file.name)
+        return response
