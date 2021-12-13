5ad758f335813d41a24e23c2da851490b9b09a3c
vusallyv <vusal581176@gmail.com>
2021-12-13 22:53:39 +0400

    Footer done

--- staff/admin.py
+++ staff/admin.py
@@ -4,37 +4,36 @@ from django.utils.html import format_html
 
 from staff.models import Teacher, Subject, PDF
 
-admin.site.register([Teacher, PDF])
+admin.site.register([Teacher, PDF, Subject])
 
-@admin.register(Subject)
-class SubjectAdmin(admin.ModelAdmin):
-    search_fields = ("title",)
-    list_display = (
-        'title', 
- 
-        )
-    readonly_fields = ("get_subjectofteacher",)
-    fields = (
-        'title', 
-        'get_subjectofteacher'
-        )
+# @admin.register(Subject)
+# class SubjectAdmin(admin.ModelAdmin):
+#     search_fields = ("title",)
+#     list_display = (
+#         'title',
 
-    def get_subjectofteacher(self, obj):
-        html = """
-        <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                    <th scope="col">adları</th>
-                    </tr>
-                </thead>
-                <tbody>
-        """
-        for i in obj.subject_teachers.all():
-            html +=f'''
-                    <tr>
-                        <td>{i.full_name}</td>
-                    </tr>
-            '''
-        return format_html(html + "</tbody></table>")
-    get_subjectofteacher.short_description = 'FBK-dakı ixtisaslar'
+#         )
+#     readonly_fields = ("get_subjectofteacher",)
+#     fields = (
+#         'title',
+#         'get_subjectofteacher'
+#         )
 
+#     def get_subjectofteacher(self, obj):
+#         html = """
+#         <table class="table">
+#                 <thead class="thead-dark">
+#                     <tr>
+#                     <th scope="col">adları</th>
+#                     </tr>
+#                 </thead>
+#                 <tbody>
+#         """
+#         for i in obj.subject_teachers.all():
+#             html +=f'''
+#                     <tr>
+#                         <td>{i.full_name}</td>
+#                     </tr>
+#             '''
+#         return format_html(html + "</tbody></table>")
+#     get_subjectofteacher.short_description = 'FBK-dakı ixtisaslar'
