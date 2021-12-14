11f03f897f8f2824788710614ceff72ce978188b
TalehIlqar <taleh.ilqar8@gmail.com>
2021-12-14 21:57:03 +0400

    bm

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-from core.models import Mostquestions
+from core.models import Mostquestions, Subscrib
 
-admin.site.register(Mostquestions)
+admin.site.register([Mostquestions, Subscrib])
